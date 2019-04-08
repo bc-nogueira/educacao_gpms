f7f43dcc23321899749e6a76f1483cbc821aa86a
bc-nogueira <bcnogueira@id.uff.br>
2019-04-08 00:21:25 -0300

    Corrige arquivo de seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-Couse.create(name: 'PHP', description: 'Aprenda a programar com php.',
+Course.create(name: 'PHP', description: 'Aprenda a programar com php.',
              syllabus: 'Html; css; js; php.', value: 10)
-Couse.create(name: 'Java', description: 'Aprenda a programar com java.',
+Course.create(name: 'Java', description: 'Aprenda a programar com java.',
              syllabus: 'Html; css; js; java.', value: 15)
-Couse.create(name: 'Ruby On Rails', description: 'Aprenda a programar com ror.',
+Course.create(name: 'Ruby On Rails', description: 'Aprenda a programar com ror.',
              syllabus: 'Html; css; js; ror.', value: 20)
