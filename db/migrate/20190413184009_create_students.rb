9c4f4a7ba813638c86de4b575fdf16edce8cf03e
bc-nogueira <bcnogueira@id.uff.br>
2019-04-13 17:08:29 -0300

    Adiciona atributo escolaridade para aluno.

--- db/migrate/20190413184009_create_students.rb
+++ db/migrate/20190413184009_create_students.rb
@@ -1,6 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
+      t.integer :scholarity
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_04_13_190645) do
   end
 
   create_table "students", force: :cascade do |t|
+    t.integer "scholarity"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
