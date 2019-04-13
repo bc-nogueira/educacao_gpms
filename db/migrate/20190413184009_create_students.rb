d5b7edf1d5131147a3bacfdca712a9452dc16ab3
Breno Nogueira <bcnogueira@id.uff.br>
2019-04-13 17:46:26 -0300

    Atualiza migration de aluno.

--- db/migrate/20190413184009_create_students.rb
+++ db/migrate/20190413184009_create_students.rb
@@ -1,7 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
-      t.integer :scholarity
       t.references :user, foreign_key: true
 
       t.timestamps
