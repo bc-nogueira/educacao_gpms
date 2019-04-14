539b39b8d41fc852996d23d4996af77646cb6698
bc-nogueira <bcnogueira@id.uff.br>
2019-04-14 12:27:03 -0300

    Configura internacionalização (i18n).

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,7 +2,7 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 
 # Whitelist locales available for the application
-I18n.available_locales = [:en, 'pt-br']
+I18n.available_locales = ['pt-BR']
 
 # Set default locale to something other than :en
-I18n.default_locale = 'pt-br'
+I18n.default_locale = 'pt-BR'
