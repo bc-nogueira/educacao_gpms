089272d3fe79ed7cbfa84d203498d93c1e7bc76a
bc-nogueira <bcnogueira@id.uff.br>
2019-04-16 01:42:31 -0300

    Fazendo busca funcionar ignorando se está com letra maiúsculas ou minúsculas.

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,7 +8,9 @@ class Course < ApplicationRecord
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :students
 
-  scope :by_name_like, ->(name) { where('name LIKE ?', "%#{name}%") }
+  scope :by_name_like, ->(name) do
+    where('LOWER(name) LIKE ?', "%#{name.downcase}%")
+  end
 
   def average_rate
     return 0 if course_ratings.empty?
