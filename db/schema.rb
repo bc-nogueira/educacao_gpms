b685834f2cdb5cb339944b51b856943f75425a3f
bc-nogueira <bcnogueira@id.uff.br>
2019-04-18 12:34:45 -0300

    Acertando visibilidade de links da aula.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_030744) do
+ActiveRecord::Schema.define(version: 2019_04_17_225813) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,16 @@ ActiveRecord::Schema.define(version: 2019_04_17_030744) do
     t.integer "course_id", null: false
     t.integer "student_id", null: false
     t.index ["course_id", "student_id"], name: "index_courses_students_on_course_id_and_student_id"
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "position"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
   end
 
   create_table "orders", force: :cascade do |t|
