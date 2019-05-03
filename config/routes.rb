5448bad8c671fc2b983cb88e82a98fc730af693f
Breno Nogueira <bcnogueira@id.uff.br>
2019-05-03 13:02:00 -0300

    Acerta routes para especificar páginas que só podem ser acessadas por usuários logados.

--- config/routes.rb
+++ config/routes.rb
@@ -2,37 +2,50 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'home#index'
 
-  resources :courses, only: [:index, :show, :new, :create, :edit, :update] do
+
+  # Routes for logged users
+  authenticate :user do
+    resources :courses, only: [:new, :create, :edit, :update]
+    resources :course_ratings, only: [:new, :create, :edit, :update, :destroy]
+    resources :lessons, only: [:show, :new, :create, :edit, :update, :destroy]
+    resources :notifications, only: [:index, :update, :destroy]
+    resources :orders, only: [:show] do
+      collection do
+        get 'student_orders'
+      end
+      member do
+        delete 'remove_course'
+        get 'add_course'
+        get 'finalize'
+        get 'pay'
+      end
+    end
+    resources :teachers, only: [:new, :create]
+    resources :teacher_ratings, only: [:new, :create, :edit, :update, :destroy]
+
+    get 'follow_teacher', to: 'students#follow_teacher'
+    get 'unfollow_teacher', to: 'students#unfollow_teacher'
+
+    # resources :students, only: [:index] do
+    #   member do
+    #     get 'follow_teacher'
+    #     get 'unfollow_teacher'
+    #   end
+    # end
+    resources :wishes, only: [:index, :create, :destroy] do
+      collection { delete 'destroy_all' }
+    end
+  end
+
+  # Routes for users that aren't logged
+  resources :courses, only: [:index, :show] do
     collection do
       get 'search'
       get 'student_courses'
       get 'teacher_courses'
     end
   end
-  resources :course_ratings, only: [:new, :create, :edit, :update, :destroy]
   resources :home, only: :index
-  resources :lessons, only: [:show, :new, :create, :edit, :update, :destroy]
-  resources :notifications, only: [:index, :update, :destroy]
-  resources :orders, only: [:show] do
-    collection do
-      get 'student_orders'
-    end
-    member do
-      delete 'remove_course'
-      get 'add_course'
-      get 'finalize'
-      get 'pay'
-    end
-  end
-  resources :students, only: :show do
-    member do
-      get 'follow_teacher'
-      get 'unfollow_teacher'
-    end
-  end
-  resources :teachers, only: [:index, :show, :new, :create]
-  resources :teacher_ratings, only: [:new, :create, :edit, :update, :destroy]
-  resources :wishes, only: [:index, :create, :destroy] do
-    collection { delete 'destroy_all' }
-  end
+  resources :students, only: [:show]
+  resources :teachers, only: [:index, :show]
 end
