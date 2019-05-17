0c3039e9100f384338bd4942b60ebb85f475496e
Breno Nogueira <bcnogueira@id.uff.br>
2019-05-17 15:37:44 -0300

    Pequenas correções no banner.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @courses = Course.all
+    @well_evaluated = @courses.select { |course| course.average_rate >= 4 }[0..4]
+    @well_evaluated.sort_by{ |course| [course.average_rate]}.reverse!
     @teachers = Teacher.all
   end
 end
--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -3,8 +3,8 @@ module CoursesHelper
     user_signed_in? && course.teacher == current_user.teacher
   end
 
-  def well_evaluated_courses(courses)
-    courses = courses.select { |course| course.average_rate >= 4 }[0..4]
-    courses.sort_by{ |course| [course.average_rate]}.reverse
-  end
+  # def well_evaluated_courses(courses)
+  #   courses = courses.select { |course| course.average_rate >= 4 }[0..4]
+  #   courses.sort_by{ |course| [course.average_rate]}.reverse
+  # end
 end
