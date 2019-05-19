54f2a5c609dec16a3753685c4c6f19c53c6804c9
bc-nogueira <bcnogueira@id.uff.br>
2019-05-18 22:37:59 -0300

    Mostra no sidebar quem está logado.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def teacher_avatar(teacher)
-    return teacher.user.avatar if teacher.user.avatar.attached?
-    return 'https://bit.ly/2EhQpzA' if teacher.gender == 'male'
+  def user_avatar(user)
+    return user.avatar if user.avatar.attached?
+    return 'https://bit.ly/2EhQpzA' if user.gender == 'male'
     'https://bit.ly/2Jt3jyO'
   end
 end
